b09fd2bc6c198bd3ae861c22edfed1fb4640191b
boboli611 <boboli611@users.noreply.github.com>
2016-06-17 16:37:55 +0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,7 @@
+<?php
+header("Content-type: text/html; charset=utf-8");
+header("Access-Control-Allow-Origin:*"); 
+//header("Access-Control-Allow-Origin: http://m.oddsstar.com"); 
+//ini_set('display_errors', 'On');
+//error_reporting( E_ALL ^ E_NOTICE ^ E_WARNING);
+?>
